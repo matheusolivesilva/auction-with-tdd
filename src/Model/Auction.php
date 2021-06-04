19dfc484f134b4bc29389bc42054968e43c4b77e
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2021-06-03 22:40:00 -0300

    Initial commit

---
+++ src/Model/Auction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Alura\Auction\Model;
+
+class Auction 
+{
+    /** @var Bid[] */
+    private $bids;
+    /** @var string */
+    private $description;
+
+    public function __construct(string $description)
+    {
+        $this->description = $description;
+        $this->bids = [];
+    }
+
+    public function receiveBid(Bid $bid)
+    {
+        $this->bids[] = $bid;
+    }
+
+    /**
+     * @return Bid[]
+     */
+    public function getBids(): array
+    {
+        return $this->bids;
+    }
+}
