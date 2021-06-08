962fb5010dcdd61d61c9cfef619060520a89945e
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2021-06-08 01:44:18 -0300

    feat: get amount of bids by user and check if bid is from the last user

--- src/Model/Auction.php
+++ src/Model/Auction.php
@@ -8,15 +8,25 @@ class Auction
     private $bids;
     /** @var string */
     private $description;
+    /** @var bool */
+    private $finished;
 
     public function __construct(string $description)
     {
         $this->description = $description;
         $this->bids = [];
+        $this->finished = false;
     }
 
     public function receiveBid(Bid $bid)
     {
+        if (!empty($this->bids) && $this->isFromLastUser($bid)) {
+            throw new \DomainException('User cannot purpose 2 followed bids');
+        }
+        $totalBidsByUser = $this->amountOfBidsByUser($bid->getUser());
+        if ($totalBidsByUser >= 5) {
+            throw new \DomainException('User cannot purpose more than 5 bids by auction');
+        }
         $this->bids[] = $bid;
     }
 
@@ -27,4 +37,40 @@ class Auction
     {
         return $this->bids;
     }
+
+    public function finalize()
+    {
+        $this->finalize = true;
+    }
+
+    public function isFinished(): bool
+    {
+        return $this->isFinished;
+    }
+
+    /**
+     * @param Bid $bid
+     * @return bool
+     */
+    private function isFromLastUser(Bid $bid): bool
+    {
+        $lastBid = $this->bids[array_key_last($this->bids)];
+        return $bid->getUser() === $lastBid->getUser();
+    }
+
+    private function amountOfBidsByUser(User $user): int
+    {
+        $totalBidsByUser = array_reduce(
+            $this->bids,
+            function (int $accumulatedTotal, Bid $currentBid) use ($user) {
+                if ($currentBid->getUser() === $user) {
+                    return $accumulatedTotal + 1;
+                }
+                return $accumulatedTotal;
+            },
+            0
+        );
+        return $totalBidsByUser;
+    }
+
 }
