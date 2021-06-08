c4a19f047f4718d6866576592c1770ea72ed78e6
Matheus Oliveira da Silva <matheus.oliveira.silv@gmail.com>
2021-06-08 03:49:51 -0300

    test: create tests for appraiser service

--- tests/Service/AppraiserTest.php
+++ tests/Service/AppraiserTest.php
@@ -9,79 +9,113 @@ use Alura\Auction\Service\Appraiser;
 
 class AppraiserTest extends TestCase
 {
-   public function testAppraiserShouldFindTheBiggestValueOfBidInDescendingOrder()
+
+   /** @var Appraiser */
+   private $appraiser;
+
+   protected function setUp(): void
    {
+       $this->appraiser = new Appraiser();
+   }
 
-        $auction = new Auction('Fiat 147 0KM');
-
-        $maria = new User('Maria');
-        $joao = new User('João');
-
-        $auction->receiveBid(new Bid($maria, 2500));
-        $auction->receiveBid(new Bid($joao, 2000));
-
-        $appraiser = new Appraiser();
-        $appraiser->evaluates($auction);
-
-        $biggestValue = $appraiser->getBiggestValue();
-
+   /**
+    * @dataProvider auctionInRandomOrder
+    * @dataProvider auctionInAscendingOrder
+    * @dataProvider auctionInDescendingOrder
+    */
+    public function testAppraiserShouldFindTheBiggestBidsValue(Auction $auction)
+    {
+        $this->appraiser->evaluates($auction);
+        $biggestValue = $this->appraiser->getBiggestValue();
         self::assertEquals(2500, $biggestValue);
-   }
+    }
 
-   public function testAppraiserShouldFindTheBiggestValueOfBidInAscendingOrder()
-   {
 
-        $auction = new Auction('Fiat 147 0KM');
+   /**
+    * @dataProvider auctionInRandomOrder
+    * @dataProvider auctionInAscendingOrder
+    * @dataProvider auctionInDescendingOrder
+    */
+    public function testAppraiserShouldFindTheSmallestBidsValue(Auction $auction)
+    {
+        $this->appraiser->evaluates($auction);
+        $smallestValue = $this->appraiser->getSmallestValue();
+        self::assertEquals(1700, $smallestValue);
+    }
 
+   /**
+    * @dataProvider auctionInRandomOrder
+    * @dataProvider auctionInAscendingOrder
+    * @dataProvider auctionInDescendingOrder
+    */
+    public function testAppraiserShouldSearch3BiggestValues(Auction $auction)
+    {
+        $this->appraiser->evaluates($auction);
+        $biggest = $this->appraiser->getBiggestBids();
+        static::assertCount(3, $biggest);
+        static::assertEquals(2500, $biggest[0]->getValue());
+        static::assertEquals(2000, $biggest[1]->getValue());
+        static::assertEquals(1700, $biggest[2]->getValue());
+    }
+
+    public function testEmptyAuctionCouldNotBeEvaluated()
+    {
+        $this->expectException(\DomainException::class);
+        $this->expectExceptionMessage('It\'s not possible to appraise an empty auction');
+        $auction = new Auction('Ferrari Testarossa');
+        $this->appraiser->evaluates($auction);
+    }
+
+    public function testFinishedAuctionCouldNotBeEvaluated()
+    {
+        $this->expectException(\DomainException::class);
+        $this->expectExceptionMessage('Auction is finished');
+        $auction = new Auction('Ferrari Testarossa');
+        $auction->receiveBid(new Bid(new User('Test'), 2000));
+        $auction->finalize();
+        $this->appraiser->evaluates($auction);
+    }
+
+    /* ----- DATA ----- */
+    public function auctionInAscendingOrder()
+    {
+        $auction = new Auction('Ferrari Testarossa');
         $maria = new User('Maria');
-        $joao = new User('João');
-
-        $auction->receiveBid(new Bid($joao, 2000));
+        $john = new User('John');
+        $ana = new User('Ana');
+        $auction->receiveBid(new Bid($ana, 1700));
+        $auction->receiveBid(new Bid($john, 2000));
         $auction->receiveBid(new Bid($maria, 2500));
+        return [
+            'ascending-order' => [$auction]
+        ];
+    }
 
-        $appraiser = new Appraiser();
-        $appraiser->evaluates($auction);
-
-        $biggestValue = $appraiser->getBiggestValue();
-
-        self::assertEquals(2500, $biggestValue);
-   }
-
-   public function testAppraiserShouldFindTheSmallestValueOfBidInAscendingOrder()
-   {
-
-        $auction = new Auction('Fiat 147 0KM');
-
+    public function auctionInDescendingOrder()
+    {
+        $auction = new Auction('Ferrari Testarossa');
         $maria = new User('Maria');
-        $joao = new User('João');
-
+        $john = new User('John');
+        $ana = new User('Ana');
         $auction->receiveBid(new Bid($maria, 2500));
-        $auction->receiveBid(new Bid($joao, 2000));
+        $auction->receiveBid(new Bid($john, 2000));
+        $auction->receiveBid(new Bid($ana, 1700));
+        return [
+            'descending-order' => [$auction]
+        ];
+    }
 
-        $appraiser = new Appraiser();
-        $appraiser->evaluates($auction);
-
-        $smallestValue = $appraiser->getSmallestValue();
-
-        self::assertEquals(2000, $smallestValue);
-   }
-
-   public function testAppraiserShouldFindTheSmallestValueOfBidInDescendingOrder()
-   {
-
-        $auction = new Auction('Fiat 147 0KM');
-
+    public function auctionInRandomOrder()
+    {
+        $auction = new Auction('Ferrari Testarossa');
         $maria = new User('Maria');
-        $joao = new User('João');
-
-        $auction->receiveBid(new Bid($joao, 2000));
+        $john = new User('John');
+        $ana = new User('Ana');
+        $auction->receiveBid(new Bid($john, 2000));
         $auction->receiveBid(new Bid($maria, 2500));
-
-        $appraiser = new Appraiser();
-        $appraiser->evaluates($auction);
-
-        $smallestValue = $appraiser->getSmallestValue();
-
-        self::assertEquals(2000, $smallestValue);
-   }
+        $auction->receiveBid(new Bid($ana, 1700));
+        return [
+            'random-order' => [$auction]
+        ];
+    }
 }
